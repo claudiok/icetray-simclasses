8f9dd8ef87df485aeb5480354bc3cc01dc8e3f4b
Alex Olivas <olivas@icecube.umd.edu>
2006-12-04 19:08:13 +0000

    importing code to the trunk of simclasses
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/simclasses/trunk@25907 16731396-06f5-0310-8873-f7f720988828

---
+++ private/simclasses/I3MCTWRParams.cxx
@@ -0,0 +1,24 @@
+#include <dataclasses/BoostHeaders.h>
+#include <simclasses/I3MCTWRParams.h>
+
+I3MCTWRParams::~I3MCTWRParams() {}
+
+template <class Archive>
+  void I3MCTWRParams::serialize(Archive& ar, unsigned version)
+  {
+    ar & make_nvp("stop_delay",stop_delay);
+    ar & make_nvp("DMADD_thresh",DMADD_thresh);
+    ar & make_nvp("TWR_thresh",TWR_thresh);
+    ar & make_nvp("rel_sens",rel_sens);
+    ar & make_nvp("wf_type",wf_type);
+    ar & make_nvp("afterpulse_prob",afterpulse_prob);
+    ar & make_nvp("afterpulse_t",afterpulse_time);
+    ar & make_nvp("noise_rate",noise_rate);
+    ar & make_nvp("amplitude",amplitude);
+    ar & make_nvp("cable_delay",cable_delay);
+    ar & make_nvp("optical",optical);
+  }
+
+
+
+I3_SERIALIZABLE(I3MCTWRParams);
