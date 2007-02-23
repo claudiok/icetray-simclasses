1d21f6332c0ee691d79ef25556960ef66d6bfeb8
Peter Niessen <niessen@ifh.de>
2007-02-23 15:58:38 +0000

    Added a class to hold the NKG info provided by corsika.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/simclasses/trunk@29614 16731396-06f5-0310-8873-f7f720988828

---
+++ private/simclasses/I3MCNKGInfo.cxx
@@ -0,0 +1,34 @@
+/*******************************************************************/
+/* I3MCNKGInfo.cxx                                                 */
+/* Implementation of the NKG info                                  */
+/* PN Fri Feb 23 10:21:09 EST 2007                                 */
+/* $Id:$ */
+/*******************************************************************/
+
+#include <dataclasses/BoostHeaders.h>
+#include <simclasses/I3MCNKGInfo.h>
+
+
+
+// a constructor, set the values to NAN
+I3MCNKGPoint::I3MCNKGPoint ()
+  : LD1 (NAN),
+    LD2 (NAN) {
+}
+
+// a virtual destructor
+I3MCNKGPoint::~I3MCNKGPoint () {
+  // wer nix weiss und wer nix kann, f"angt zu simulieren an.
+}
+
+// the streamer necessary for boosting it to a file
+template <class Archive> void I3MCNKGPoint::serialize (Archive &ar,
+						      unsigned version) {
+
+  ar &make_nvp ("LD1", LD1);
+  ar &make_nvp ("LD2", LD2);
+
+}
+
+
+I3_SERIALIZABLE (I3MCNKGPoint);
---
+++ public/simclasses/I3MCNKGInfo.h
@@ -0,0 +1,58 @@
+/**
+ *
+ * @file I3MCNKGInfo.h
+ *
+ * @brief Store the data points for the NKG samples provided by CORSIKA
+ * @author Peter Niessen Fri Feb 23 10:09:26 EST 2007
+ * @date $Date:$
+ * @version $Release:$
+ *
+ * copyright (C) 2007
+ * the IceCube collaboration
+ *
+ * $Id:$
+ */
+
+// multiple include protection
+#ifndef I3MNKGINFO_H_INCLUDED
+#define I3MNKGINFO_H_INCLUDED
+
+#include <dataclasses/Utility.h>
+#include <dataclasses/I3Position.h>
+
+struct I3MCNKGPoint {
+
+  /**
+   * see CORSIKA manual for details on this
+   */
+  double LD1; /// lateral distribution density observation level 1, in m^-2
+  double LD2; /// lateral distribution density observation level 2, in m^-2
+
+  /**
+   * a constructor;
+   * sets contents to NAN
+   */
+  I3MCNKGPoint ();
+
+  /**
+   * a virtual destructor
+   * weiss nix? Macht nix!
+   */
+  virtual ~I3MCNKGPoint ();
+
+  /**
+   * the streamer necessary for boosting it to a file
+   */
+  template <class Archive> void serialize (Archive &ar, unsigned version);
+
+};
+
+/**
+ * aggregate many points into a map
+ */
+typedef std::map<I3Position, I3MCNKGPoint> I3MCNKGInfoMap;
+
+I3_POINTER_TYPEDEFS (I3MCNKGPoint);
+I3_POINTER_TYPEDEFS (I3MCNKGInfoMap);
+
+#endif
