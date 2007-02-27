113e8b1cc7f8c4381c95fe1b2b279892ce296f2a
Peter Niessen <niessen@ifh.de>
2007-02-27 21:08:15 +0000

    Started on the interpolation business.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/simclasses/trunk@29798 16731396-06f5-0310-8873-f7f720988828

--- private/simclasses/I3MCNKGInfo.cxx
+++ private/simclasses/I3MCNKGInfo.cxx
@@ -10,6 +10,8 @@
 
 
 
+/*******************************************************************/
+
 // a constructor, set the values to NAN
 I3MCNKGPoint::I3MCNKGPoint ()
   : Position (NAN, NAN, NAN),
@@ -17,10 +19,14 @@ I3MCNKGPoint::I3MCNKGPoint ()
     LD2 (NAN) {
 }
 
+/*******************************************************************/
+
 // a virtual destructor
 I3MCNKGPoint::~I3MCNKGPoint () {
   // wer nix weiss und wer nix kann, f"angt zu simulieren an.
 }
+
+/*******************************************************************/
 
 // the streamer necessary for boosting it to a file
 template <class Archive> void I3MCNKGPoint::serialize (Archive &ar,
@@ -32,6 +38,100 @@ template <class Archive> void I3MCNKGPoint::serialize (Archive &ar,
 
 }
 
+/*******************************************************************/
+
 
 I3_SERIALIZABLE (I3MCNKGPoint);
 I3_SERIALIZABLE (I3MCNKGInfoList);
+
+/*******************************************************************/
+/* here follows the interpolation implementation                   */
+/*******************************************************************/
+
+
+// constructor
+I3MCNKGInterpolation::I3MCNKGInterpolation (const I3MCNKGInfoList &nkg_values,
+					    const I3Particle &primary) {
+
+  fillGrid (nkg_values, primary);
+
+}
+
+/*******************************************************************/
+
+// destructor
+I3MCNKGInterpolation::~I3MCNKGInterpolation () {
+}
+  
+/*******************************************************************/
+
+// fill the grid to enable the interpolations. Calculate the first and
+// second derivatives as well
+void I3MCNKGInterpolation::fillGrid (const I3MCNKGInfoList &nkg_values,
+				     const I3Particle &primary) {
+
+  // we store the logarithm (natural, to avoid the non-existing 10^)
+  // of the function value as a function of log (r/r_0) and phi.
+
+  // Don't forget to subtract the core position!
+
+  // some ugly hardcoding: In corsika, we have 10 r bins and 8 phi
+  // bins.
+  static const int N_R = 10;
+  static const int N_PHI = 8;
+
+  // r_0 is the smallest value, usually the negative of the first nkg
+  // values. The largest value is at the tenth position
+
+  double r_0 = -nkg_values[0].Position.GetX ()
+    - primary.GetPos ().GetX ();
+  double r_max = -nkg_values[N_R-1].Position.GetX ()
+    - primary.GetPos ().GetX ();
+
+  // here again, we need to hardcode by the values of atan2 (y, x)
+  // which is +pi maximally and -3/4 pi minimally
+  double phi_0 = atan2 (-1., -1.); // -3/4 pi
+  double phi_max = atan2 (0., -1.); // pi
+
+  double x; // carthesian coordinates
+  double y;
+  double r; // polar coordinates
+  double phi;
+  double l; // logarithm of the density
+
+  f_table_index_t coords; // the coordinates of a position
+
+  for (I3MCNKGInfoList::const_iterator i_nkg = nkg_values.begin ();
+       i_nkg != nkg_values.end ();
+       i_nkg++) {
+
+    l = log (i_nkg->LD1);
+    x = i_nkg->Position.GetX () - primary.GetPos ().GetX ();
+    y = i_nkg->Position.GetY () - primary.GetPos ().GetY ();
+
+    r = sqrt (x * x + y * y);
+    phi = atan2 (y, x);
+
+    // this makes the equidistant and eases interpolation
+    coords[0] = N_R * int ((log (r/r_0) - log (r_0 / r_0))
+			   / (log (r_max / r_0) - log (r_0 / r_0)));
+    coords[1] = N_PHI * int ((phi - phi_0) / (phi_max - phi_0));
+
+    f_nkg[coords] = l;
+
+  }
+
+  // now we have the function values ordered by rho = log (r/r_0) and
+  // phi. Let's start calculating the derivatives so that we can
+  // interpolate
+  // the first derivative
+  
+  for (f_table_t::const_iterator i_nkg = f_nkg.begin ();
+       i_nkg != f_nkg.end ();
+       i_nkg++) {
+
+  }
+
+}
+
+  
--- public/simclasses/I3MCNKGInfo.h
+++ public/simclasses/I3MCNKGInfo.h
@@ -17,9 +17,13 @@
 #ifndef I3MNKGINFO_H_INCLUDED
 #define I3MNKGINFO_H_INCLUDED
 
+#include <vector>
+#include <map>
+
 #include <dataclasses/Utility.h>
 #include <dataclasses/I3Position.h>
 #include <dataclasses/I3Vector.h>
+#include <dataclasses/physics/I3Particle.h>
 
 struct I3MCNKGPoint {
 
@@ -57,6 +61,43 @@ struct I3MCNKGPoint {
  * aggregate many points into a map
  */
 typedef I3Vector<I3MCNKGPoint> I3MCNKGInfoList;
+
+/**
+ * A class to do the interpolation of values
+ */
+class I3MCNKGInterpolation {
+
+ public:
+
+  // constructor
+  I3MCNKGInterpolation (const I3MCNKGInfoList &nkg_values,
+			const I3Particle &primary);
+
+  // destructor
+  ~I3MCNKGInterpolation ();
+  
+
+ private:
+
+  // need some typedefs
+  typedef std::vector<int> f_table_index_t;
+  typedef std::map<f_table_index_t, double> f_table_t;
+
+  // the function value of the nkg density as well as the derivatives
+  // necessary for interpolation.
+  f_table_t f_nkg;
+  f_table_t df_dr;
+  f_table_t df_dphi;
+  f_table_t d2f_dr_dr;
+  f_table_t d2f_dr_dphi;
+  f_table_t d2f_dphi_dphi;
+
+  // fill the grid to enable the interpolations
+  void fillGrid (const I3MCNKGInfoList &nkg_values,
+		 const I3Particle &primary);
+
+  
+};
 
 I3_POINTER_TYPEDEFS (I3MCNKGPoint);
 I3_POINTER_TYPEDEFS (I3MCNKGInfoList);
