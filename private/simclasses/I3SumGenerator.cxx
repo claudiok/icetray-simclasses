e412471332a5c78e0f4aac81ca8d3b1f9ee29c9e
Klas Hultqvist <klas.hultqvist@fysik.su.se>
2008-02-21 07:09:18 +0000

    Sumgenerator bugfix only worked for single term (does not affect any current use). Fixed this for multiple terms.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/simclasses/trunk@42474 16731396-06f5-0310-8873-f7f720988828

--- private/simclasses/I3SumGenerator.cxx
+++ private/simclasses/I3SumGenerator.cxx
@@ -139,8 +139,8 @@ void I3SumGenerator::Initialise(I3RandomServicePtr r,double (*fun)(double),
       while (P[terms][binxAfter] < prob) binxAfter++;
       binxBefore=binxAfter-1;
       // Find x value for current probability value by interpolation
-      X_[terms][binP] = xlo + terms*dx*( binxBefore + (prob-P[terms][binxBefore])/
-				   (P[terms][binxAfter] - P[terms][binxBefore]) );
+      X_[terms][binP] = terms*(xlo + dx*( binxBefore + (prob-P[terms][binxBefore])/
+				   (P[terms][binxAfter] - P[terms][binxBefore]) ));
     }
     X_[terms][nBins_] = terms*xhi;
     /**
@@ -161,8 +161,8 @@ void I3SumGenerator::Initialise(I3RandomServicePtr r,double (*fun)(double),
       prob=prob*prob*prob;
       while (P[terms][binxAfter] <= prob) binxAfter++;
       binxBefore=binxAfter-1;
-      XLow_[terms][binP] = xlo + terms*dx*(binxBefore + (prob-P[terms][binxBefore])/
-				     (P[terms][binxAfter] - P[terms][binxBefore]));
+      XLow_[terms][binP] = terms*(xlo + dx*(binxBefore + (prob-P[terms][binxBefore])/
+				     (P[terms][binxAfter] - P[terms][binxBefore])));
     }
     // Add extra element in case of rounding problems at generation:
     XLow_[terms][nBinsLow_+1] = XLow_[terms][nBinsLow_];
@@ -178,8 +178,8 @@ void I3SumGenerator::Initialise(I3RandomServicePtr r,double (*fun)(double),
       prob=1-prob*prob*prob;
       while (P[terms][binxBefore] > prob) binxBefore--;
       binxAfter=binxBefore+1;
-      XHigh_[terms][nBinsHigh_-binP] = xlo + terms*dx*( binxBefore + (prob-P[terms][binxBefore])/
-						  (P[terms][binxAfter] - P[terms][binxBefore]));
+      XHigh_[terms][nBinsHigh_-binP] = terms*(xlo + dx*( binxBefore + (prob-P[terms][binxBefore])/
+						  (P[terms][binxAfter] - P[terms][binxBefore])));
     }
     // For rounding errors:
     XHigh_[terms][nBinsHigh_+1] = XHigh_[terms][nBinsHigh_];
