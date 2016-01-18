dd6828e47ae5c6be5aadf42e7d573acad2e59b7f
Benedikt Riedel <benedikt.riedel@icecube.wisc.edu>
2016-01-18 23:15:00 +0000

    Adding suggestions from cweaver
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/simclasses/branches/mcpeseriesmap_utils@140992 16731396-06f5-0310-8873-f7f720988828

--- private/simclasses/I3MCPEUtils.cxx
+++ private/simclasses/I3MCPEUtils.cxx
@@ -1,30 +1,23 @@
 #include "simclasses/I3MCPEUtils.h"
-/* 
- ********************************************************************
- * MergeNoSort                                                
- * Merges two I3MCPEMaps without sorting MCPESeries in time
- * Shamelessly adapted from vuvuzela.
- *
- *  \param originalMap The MCPE series map to merge into
- *  \param mergeMap The MCPE series map from the frame
- ******************************************************************** 
- */ 
+
 void I3MCPESeriesMapUtils::MergeNoSort(I3MCPESeriesMap& originalMap, const I3MCPESeriesMap& mergeMap)
 {
     I3MCPESeriesMap::const_iterator itMerge;
-    // Loop over the noise hits
     for(itMerge = mergeMap.begin(); itMerge != mergeMap.end(); ++itMerge)
     {
         I3MCPESeriesMap::const_iterator itOriginal = originalMap.find(itMerge->first);
         if (itOriginal != originalMap.end())
         {
             assert(!(itMerge->second.empty()));
-            const I3MCPESeries& mergeMCPESeries = itMerge->second;
-            I3MCPESeries::const_iterator mergeMCPEIter;
-            for(mergeMCPEIter = mergeMCPESeries.begin(); 
-                mergeMCPEIter != mergeMCPESeries.end(); 
-                ++mergeMCPEIter) 
-              originalMap[itMerge->first].push_back(*mergeMCPEIter);
+            itOriginal->second.insert(itOriginal->second.end(),
+                                      itMerge->second.begin(),
+                                      itMerge->second.end());
+            // const I3MCPESeries& mergeMCPESeries = itMerge->second;
+            // I3MCPESeries::const_iterator mergeMCPEIter;
+            // for(mergeMCPEIter = mergeMCPESeries.begin();
+            //     mergeMCPEIter != mergeMCPESeries.end();
+            //     ++mergeMCPEIter)
+            //   originalMap[itMerge->first].push_back(*mergeMCPEIter);
             
         }
         else if (!(itMerge->second.empty()))
@@ -34,19 +27,16 @@ void I3MCPESeriesMapUtils::MergeNoSort(I3MCPESeriesMap& originalMap, const I3MCP
     }
 }
 
-bool CompareMCPEs(const I3MCPE& lhs, const I3MCPE& rhs){
-  return lhs.time < rhs.time;
+namespace
+{
+    bool CompareMCPEs(const I3MCPE& lhs, const I3MCPE& rhs)
+    {
+        return lhs.time < rhs.time;
+    }
 }
 
-/* 
- ********************************************************************
- * SortMCPESeriesMap                                                
- * Sort MCPESeries of a MCPESeriesMap in time. 
- * Shamelessly adapted from vuvuzela.
- *
- *  \param unsortedMap MCPE series map not sorted in time
- ******************************************************************** 
- */ 
+
+
 void I3MCPESeriesMapUtils::SortMCPESeriesMap(I3MCPESeriesMap& unsortedMap)
 {
     I3MCPESeriesMap::iterator it;
@@ -54,16 +44,6 @@ void I3MCPESeriesMapUtils::SortMCPESeriesMap(I3MCPESeriesMap& unsortedMap)
         sort((it->second).begin(), (it->second).end(), CompareMCPEs);
 }
 
-/* 
- ********************************************************************
- * Merge                                                
- * Merges two I3MCPEMaps . 
- * Shamelessly adapted from vuvuzela.
- *
- *  \param originalMap The MCPE series map to merge into
- *  \param mergeMap The MCPE series map from the frame
- ******************************************************************** 
- */ 
 void I3MCPESeriesMapUtils::Merge(I3MCPESeriesMap& originalMap, const I3MCPESeriesMap& mergeMap)
 {
     I3MCPESeriesMapUtils::MergeNoSort(originalMap, mergeMap);
@@ -71,17 +51,6 @@ void I3MCPESeriesMapUtils::Merge(I3MCPESeriesMap& originalMap, const I3MCPESerie
     
 }
 
-/* 
- ********************************************************************
- * MergeReturn                                                
- * Merges two I3MCPEMaps . 
- * Shamelessly adapted from vuvuzela.
- *
- *  \param originalMap The MCPE series map to merge into
- *  \param mergeMap The MCPE series map from the frame
- *  \return New merged MCPE series map
- ******************************************************************** 
- */ 
 I3MCPESeriesMapConstPtr I3MCPESeriesMapUtils::MergeReturn(const I3MCPESeriesMap& originalMap, 
                                                  const I3MCPESeriesMap& mergeMap)
 {
--- public/simclasses/I3MCPEUtils.h
+++ public/simclasses/I3MCPEUtils.h
@@ -5,13 +5,44 @@
 
 namespace I3MCPESeriesMapUtils
 {
-
+   /* 
+    * MergeNoSort
+    * Merges two I3MCPEMaps without sorting MCPESeries in time
+    * Shamelessly adapted from vuvuzela.
+    *
+    *  \param originalMap The MCPE series map to merge into
+    *  \param mergeMap The MCPE series map from the frame
+    */ 
     void MergeNoSort(I3MCPESeriesMap&, const I3MCPESeriesMap&);
-
+    
+   /* 
+    * Merge
+    * Merges two I3MCPEMaps . 
+    * Shamelessly adapted from vuvuzela.
+    *
+    *  \param originalMap The MCPE series map to merge into
+    *  \param mergeMap The MCPE series map from the frame
+    */ 
     void Merge(I3MCPESeriesMap&, const I3MCPESeriesMap&);
 
+   /* 
+    * MergeReturn
+    * Merges two I3MCPEMaps . 
+    * Shamelessly adapted from vuvuzela.
+    *
+    *  \param originalMap The MCPE series map to merge into
+    *  \param mergeMap The MCPE series map from the frame
+    *  \return New merged MCPE series map
+    */ 
     I3MCPESeriesMapConstPtr MergeReturn(const I3MCPESeriesMap& originalMap, const I3MCPESeriesMap& mergeMap);
-
+   
+   /* 
+    * SortMCPESeriesMap
+    * Sort MCPESeries of a MCPESeriesMap in time. 
+    * Shamelessly adapted from vuvuzela.
+    *
+    *  \param unsortedMap MCPE series map not sorted in time
+    */ 
     void SortMCPESeriesMap(I3MCPESeriesMap&);
 
 }
